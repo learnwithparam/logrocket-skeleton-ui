78c15249ba47cef5d42cd358e604ea6bcc530bfe
Paramanantham Harrison <paramanantham@live.com>
2019-09-15 22:07:52 +0300

    feat: skeleton UI (#3)
    * feat: installing react skeleton package
    * feat: example with skeleton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+
 import youtubeData from "./data";
 
 import "./App.css";
@@ -35,29 +37,64 @@ const CardList = ({ list }) => {
   );
 };
 
+// Skeleton component
+const CardSkeleton = () => {
+  return (
+    <section>
+      <h2 className="section-title">
+        <Skeleton height={28} width={300} />
+      </h2>
+
+      <ul className="list">
+        {Array(9)
+          .fill()
+          .map((item, index) => (
+            <li className="card" key={index}>
+              <Skeleton height={180} />
+              <h4 className="card-title">
+                <Skeleton height={36} width={`80%`} />
+              </h4>
+              <p className="card-channel">
+                <Skeleton width={`60%`} />
+              </p>
+              <div className="card-metrics">
+                <Skeleton width={`90%`} />
+              </div>
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
+};
+
 const App = () => {
   const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   // Load this effect on mount
   useEffect(() => {
+    setLoading(true);
     const timer = setTimeout(() => {
       setVideos(youtubeData);
-    }, 3000);
+      setLoading(false);
+    }, 2000);
     // Cancel the timer while unmounting
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <div className="App">
-      {videos.map((list, index) => {
-        return (
-          <section key={index}>
-            <h2 className="section-title">{list.section}</h2>
-            <CardList list={list} />
-            <hr />
-          </section>
-        );
-      })}
+      {loading && <CardSkeleton />}
+      {!loading &&
+        videos.map((list, index) => {
+          return (
+            <section key={index}>
+              <h2 className="section-title">{list.section}</h2>
+              <CardList list={list} />
+              <hr />
+            </section>
+          );
+        })}
     </div>
   );
 };
