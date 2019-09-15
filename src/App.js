bbcc32f81bd5cc5a9b1adf5f38fcae270f6a9f19
Paramanantham Harrison <paramanantham@live.com>
2019-09-15 17:04:11 +0300

    feat: youtube basic UI (#1)
    * feat: youtube UI
    * fix: data value for section name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import youtubeData from "./data";
+
+import "./App.css";
+
+const Card = ({ item, channel }) => {
+  return (
+    <li className="card">
+      <a
+        href={`https://www.youtube.com/watch?v=${item.id}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="card-link"
+      >
+        <img src={item.image} alt={item.title} className="card-image" />
+        <h4 className="card-title">{item.title}</h4>
+        <p className="card-channel">
+          <i>{channel}</i>
+        </p>
+        <div className="card-metrics">
+          {item.views} &bull; {item.published}
+        </div>
+      </a>
+    </li>
+  );
+};
+
+const CardList = ({ list }) => {
+  return (
+    <ul className="list">
+      {list.items.map((item, index) => {
+        return <Card key={index} item={item} channel={list.channel} />;
+      })}
+    </ul>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {youtubeData.map((list, index) => {
+        return (
+          <section key={index}>
+            <h2 className="section-title">{list.section}</h2>
+            <CardList list={list} />
+            <hr />
+          </section>
+        );
+      })}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,79 @@
+const youtubeData = [
+  {
+    section: "JavaScript Tutorials by freeCodeCamp",
+    channel: "freeCodeCamp.org",
+    items: [
+      {
+        id: "PkZNo7MFNFg",
+        image: "https://img.youtube.com/vi/PkZNo7MFNFg/maxresdefault.jpg",
+        title: "Learn JavaScript - Full Course for Beginners",
+        views: "1.9M views",
+        published: "9 months ago"
+      },
+      {
+        id: "jaVNP3nIAv0",
+        image: "https://img.youtube.com/vi/jaVNP3nIAv0/maxresdefault.jpg",
+        title: "JavaScript, HTML, CSS - Rock Paper Scissors Game",
+        views: "216K views",
+        published: "1 year ago"
+      },
+      {
+        id: "t2CEgPsws3U",
+        image: "https://img.youtube.com/vi/t2CEgPsws3U/maxresdefault.jpg",
+        title: "Data Structures and Algorithms in JavaScript",
+        views: "547K views",
+        published: "2 year ago"
+      },
+      {
+        id: "m_HJ3juuFvo",
+        image: "https://img.youtube.com/vi/m_HJ3juuFvo/maxresdefault.jpg",
+        title: "Javascript Project Tutorial: Budget App",
+        views: "75K views",
+        published: "9 months ago"
+      },
+      {
+        id: "3EMxBkqC4z0",
+        image: "https://img.youtube.com/vi/3EMxBkqC4z0/maxresdefault.jpg",
+        title: "Intro to Game Development with JavaScript",
+        views: "1.1M views",
+        published: "3 months ago"
+      },
+      {
+        id: "qI43yX1QeLM",
+        image: "https://img.youtube.com/vi/qI43yX1QeLM/maxresdefault.jpg",
+        title: "The Complete React Web Developer Course",
+        views: "7k views",
+        published: "11 months ago"
+      }
+    ]
+  },
+  {
+    section: "Small steps on React",
+    channel: "Learn with Param",
+    items: [
+      {
+        id: "ylbVzIBhDIM",
+        image: "https://img.youtube.com/vi/ylbVzIBhDIM/maxresdefault.jpg",
+        title: "useState example by building a text-size changer",
+        views: "148 views",
+        published: "3 days ago"
+      },
+      {
+        id: "ZUs7z7jxSH0",
+        image: "https://img.youtube.com/vi/ZUs7z7jxSH0/maxresdefault.jpg",
+        title: "Fetch API data in React using useEffect hooks",
+        views: "193 views",
+        published: "6 days ago"
+      },
+      {
+        id: "0man5ImAulg",
+        image: "https://img.youtube.com/vi/0man5ImAulg/maxresdefault.jpg",
+        title: "Managing error and loading state using useReducer",
+        views: "70 views",
+        published: "1 day ago"
+      }
+    ]
+  }
+];
+
+export default youtubeData;
